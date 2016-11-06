8553a072475e5a6e0d9dc5aa2219a8b70cfa91b9
Patrick Koller <kollerpat@gmail.com>
2016-11-06 19:48:09 +0100

    asf

--- evm/src/main/java/com/websystique/springmvc/configuration/CORSFilter.java
+++ evm/src/main/java/com/websystique/springmvc/configuration/CORSFilter.java
@@ -10,7 +10,7 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
 
-//test
+//test1
 
 public class CORSFilter implements Filter {
 
