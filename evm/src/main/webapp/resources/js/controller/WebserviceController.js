621fe7d83d7c8be75d3db8fc5ab2e3faa09f9b6a
Patrick Koller <kollerpat@gmail.com>
2016-11-16 21:52:44 +0100

    Simples Auslesen eines JSON Objektes.

--- evm/src/main/webapp/resources/js/controller/WebserviceController.js
+++ evm/src/main/webapp/resources/js/controller/WebserviceController.js
@@ -1,20 +1,37 @@
 'use strict';
 
 App.controller("WebserviceController", function($scope) {
-    $scope.products = ["Milk", "Bread", "Cheese"];
-   
-    $scope.addItem = function () {
-        $scope.errortext = "";
-        if (!$scope.addMe) {return;}
-        if ($scope.products.indexOf($scope.addMe) == -1) {
-            $scope.products.push($scope.addMe);
-        } else {
-            $scope.errortext = "The item is already in your shopping list.";
-        }
-    }
-    
-    $scope.removeItem = function (x) {
-        $scope.errortext = "";
-        $scope.products.splice(x, 1);
-    } 
+	$scope.products = [ "Item 1", "Item 2", "Item 3" ];
+
+	$scope.jsonObject = {
+		"personen" : [ {
+			"Name" : "Patrick",
+			"Vorname" : "Koller"
+		}, {
+			"Name" : "Michael",
+			"Vorname" : "Koller"
+		}, {
+			"Name" : "Heinz",
+			"Vorname" : "Koller"
+		}, {
+			"Name" : "Lotti",
+			"Vorname" : "Koller"
+		} ]
+	};
+
+	$scope.addItem = function() {
+		$scope.errortext = "";
+		if (!$scope.addMe) {
+			return;
+		}
+		if ($scope.products.indexOf($scope.addMe) == -1) {
+			$scope.products.push($scope.addMe);
+		} else {
+			$scope.errortext = "The item is already in your shopping list.";
+		}
+	}
+	$scope.removeItem = function(x) {
+		$scope.errortext = "";
+		$scope.products.splice(x, 1);
+	}
 });
