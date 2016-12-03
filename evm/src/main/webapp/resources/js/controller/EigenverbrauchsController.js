3aa5d1615ee33cf11c904ceaa843c0e0f9b2e948
Patrick Koller <kollerpat@gmail.com>
2016-12-03 12:55:01 +0100

    asdf

---
+++ evm/src/main/webapp/resources/js/controller/EigenverbrauchsController.js
@@ -0,0 +1,14 @@
+'use strict';
+
+App.controller("EigenverbrauchsController", function($scope, $http) {
+	
+	/*
+	$scope.getstatisticData= function() {
+	    $http.get('http://localhost:8080/EM/App/getCurrentStatistics').
+        then(function(response) {
+            $scope.statisticData = response.data;
+        });
+	}
+	*/
+				
+});
